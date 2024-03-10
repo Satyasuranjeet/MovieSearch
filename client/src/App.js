d9c6e313417cac4f56c30827e16be67ea593b896
satyasuranjeet <satyajena911@gmail.com>
2024-03-10 19:18:44 +0530

    fixed the url

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=90a30c45";
+const API_URL = "https://www.omdbapi.com?apikey=90a30c45";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
